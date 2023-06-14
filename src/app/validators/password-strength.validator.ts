9c9192c7030f2bded7e586da7d22e175071e835c
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2023-06-14 16:33:13 +0200

    create custom validator

---
+++ src/app/validators/password-strength.validator.ts
@@ -0,0 +1,7 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from "@angular/forms";
+
+export function createPasswordStrengthValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+
+  }
+}
