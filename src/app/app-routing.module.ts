00470bb6e9624e09e164337300ad8852854c4c14
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2023-06-27 11:55:59 +0200

    use reactive form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {AboutComponent} from './about/about.component';
-import {CourseComponent} from './course/course.component';
-import {courseResolver} from './services/course.resolver';
-import {LoginComponent} from './login/login.component';
-import {CreateCourseComponent} from './create-course/create-course.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CourseComponent } from './course/course.component';
+import { courseResolver } from './services/course.resolver';
+import { LoginComponent } from './login/login.component';
+import { LoginReactiveComponent } from './login-reactive/login-reactive.component';
+import { CreateCourseComponent } from './create-course/create-course.component';
 
 const routes: Routes = [
   {
@@ -30,7 +31,7 @@ const routes: Routes = [
   },
   {
     path: 'login',
-    component: LoginComponent
+    component: LoginReactiveComponent
   },
   {
     path: '**',
