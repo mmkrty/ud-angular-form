72c7460c50ff658122f88953ea7ba03fb4da6a53
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2023-06-27 15:44:33 +0200

    implement error messages

--- src/app/login-reactive/login-reactive.component.ts
+++ src/app/login-reactive/login-reactive.component.ts
@@ -27,4 +27,12 @@ export class LoginReactiveComponent implements OnInit {
 
   }
 
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
 }
