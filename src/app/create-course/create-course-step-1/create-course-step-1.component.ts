1e855c4f159deecda24b6d80ae43cbca5e202726
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2023-06-28 16:32:21 +0200

    setup step one component

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {CoursesService} from '../../services/courses.service';
-import {Observable} from 'rxjs';
-import {filter} from 'rxjs/operators';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { CoursesService } from '../../services/courses.service';
+import { Observable } from 'rxjs';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'create-course-step-1',
@@ -11,6 +11,17 @@ import {filter} from 'rxjs/operators';
 })
 export class CreateCourseStep1Component implements OnInit {
 
+  form = this.fb.group({
+    title: ['', [
+      Validators.required,
+      Validators.minLength(5),
+      Validators.maxLength(60)
+    ]],
+  })
+
+  constructor(private fb: FormBuilder, private coursesService: CoursesService) {
+
+  }
   ngOnInit() {
 
   }
