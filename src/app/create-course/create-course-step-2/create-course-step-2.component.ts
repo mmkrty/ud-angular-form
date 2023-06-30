aef535fe3e9ccf6f87fcca5eab6c63ae77e226be
Hao Chen Liu <haochenliu@Haos-Laptop-4.local>
2023-06-30 10:56:58 +0200

    enable and disable input fields according to the value of other input fields

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -11,7 +11,12 @@ export class CreateCourseStep2Component implements OnInit {
 
   form = this.fb.group({
     courseType: ['premium', Validators.required],
-
+    price: [null, [
+      Validators.required,
+      Validators.min(1),
+      Validators.max(9999),
+      Validators.pattern('[0-9]+')
+    ]],
   })
 
   constructor(private fb: FormBuilder) {
@@ -19,7 +24,16 @@ export class CreateCourseStep2Component implements OnInit {
   }
 
   ngOnInit() {
+    this.form.valueChanges
+      .subscribe(val => {
+        const priceControl = this.form.controls["price"];
 
+        if (val.courseType === 'free' && priceControl.enabled) {
+          priceControl.disable({ emitEvent: false });
+        } else if (val.courseType === 'premium' && priceControl.disabled) {
+          priceControl.enable({ emitEvent: false });
+        }
+      })
 
 
   }
